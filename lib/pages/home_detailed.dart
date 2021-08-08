1def7080f44b80228693fee9040dd7e8ea75a231
Maria Magana <maria.imagana15@gmail.com>
2021-08-07 23:01:07 -0700

    switched radio to drop down menu

--- lib/pages/home_detailed.dart
+++ lib/pages/home_detailed.dart
@@ -1,30 +1,6 @@
 import 'package:flutter/material.dart';
 
-// class ScanItemDetailed extends StatelessWidget {
-//   final String name;
-//   final String email;
-
-//   ScanItemDetailed({required this.name, required this.email});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         // text doesn't show up
-//         title: Text('Test 2'),
-//       ),
-//       body: Container(
-//           child: ListTile(
-//         title: Text(name),
-//         subtitle: Text(email),
-//       )),
-//     );
-//   }
-// }
-
-enum PackageType { box, pack, unit, can, bottle }
-
-/// This is the stateful widget that the main application instantiates.
+// Dropdown menu for type of package.
 class PackageTypeWidget extends StatefulWidget {
   const PackageTypeWidget({Key? key}) : super(key: key);
 
@@ -32,75 +8,75 @@ class PackageTypeWidget extends StatefulWidget {
   State<PackageTypeWidget> createState() => _PackageTypeWidgetState();
 }
 
-/// This is the private State class that goes with MyStatefulWidget.
 class _PackageTypeWidgetState extends State<PackageTypeWidget> {
-  PackageType? _character = PackageType.box;
+
+  String dropdownValue = 'Pack';
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListTile(
-          title: const Text('Box'),
-          leading: Radio<PackageType>(
-            value: PackageType.box,
-            groupValue: _character,
-            onChanged: (PackageType? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
-        ),
-        ListTile(
-          title: const Text('Pack'),
-          leading: Radio<PackageType>(
-            value: PackageType.pack,
-            groupValue: _character,
-            onChanged: (PackageType? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
-        ),
-        ListTile(
-          title: const Text('Single'),
-          leading: Radio<PackageType>(
-            value: PackageType.unit,
-            groupValue: _character,
-            onChanged: (PackageType? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
-        ),
-        ListTile(
-          title: const Text('Single'),
-          leading: Radio<PackageType>(
-            value: PackageType.can,
-            groupValue: _character,
-            onChanged: (PackageType? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
-        ),
-        ListTile(
-          title: const Text('Single'),
-          leading: Radio<PackageType>(
-            value: PackageType.bottle,
-            groupValue: _character,
-            onChanged: (PackageType? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
-        ),
-      ],
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.takeout_dining),
+      iconSize: 24,
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['Case', 'Pack', 'Single', 'Bottle', 'Can']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 }
+
+// Dropdown menu for item location.
+class ItemLocationWidget extends StatefulWidget {
+  const ItemLocationWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ItemLocationWidget> createState() => _ItemLocationWidgetState();
+}
+
+class _ItemLocationWidgetState extends State<ItemLocationWidget> {
+
+  String dropdownValue = 'Pack';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.takeout_dining),
+      iconSize: 24,
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['Case', 'Pack', 'Single', 'Bottle', 'Can']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
